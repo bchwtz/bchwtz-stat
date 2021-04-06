aad443b99fde39f8d0ca79b1546e75766043298e
bchwtz <benjamin.buchwitz@gmail.com>
2021-04-06 17:29:45 +0200

    Update

--- slides/makegif.sh
+++ slides/makegif.sh
@@ -2,4 +2,4 @@
 
 convert -delay 250 -geometry 480x360 00-Intro.pdf gifs/00-Intro.gif
 convert -delay 250 -geometry 480x360 01-Begriffe.pdf gifs/01-Begriffe.gif
-convert -delay 250 -geometry 480x360 02-Haeufigkeit.pdf gifs/02-Haeufigkeiten.gif
+convert -delay 250 -geometry 480x360 02-Haeufigkeit.pdf gifs/02-Haeufigkeit.gif
